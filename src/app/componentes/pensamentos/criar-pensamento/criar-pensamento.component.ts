9d185aaddd42bf132f6b9eb62d8336193b00c72d
jhordan <jhordan.suzuki@followize.com.br>
2022-12-05 19:35:27 -0300

    add data binding

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  criarPensamento() {
+    alert('Novo pensamento criado')
+  }
+
+  cancelarPensamento(){
+    alert('Pensamento cancelado')
+  }
+
 }
