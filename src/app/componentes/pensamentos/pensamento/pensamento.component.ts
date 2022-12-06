426901597a9a5f1997631f2d7847ea0d08d4c7a4
jhordan <jhordan.suzuki@followize.com.br>
2022-12-06 10:24:06 -0300

    add pensamentos

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+  pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Jhordan Suzuki',
+    modelo: 'modelo3'
+  }
+
+}
